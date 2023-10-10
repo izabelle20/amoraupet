7085666f1d3a0dd24ea1a27831397b9668c9a9f0
Izabelle ferreira da silva <127629684+izabelle20@users.noreply.github.com>
2023-10-10 20:07:42 -0300

    Add files via upload

---
+++ cachorro-prod-brinquedo.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>AMOR AU PET</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <link href="img/favicon.ico" rel="icon">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet"> 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Cabeçalho -->
+    <div class="container-fluid">
+            <div class="row py-3 px-lg-5">
+                <div class="col-lg-4">
+                    <a href="" class="navbar-brand d-none d-lg-block">
+                        <h1 class="m-0 display-5 text-capitalize"><span class="text-primary">Amor</span> Au Pet</h1>
+                    </a>
+                </div>
+                <div class="col-lg-8 text-center text-lg-right">
+                    <div class="d-inline-flex align-items-center">
+                        <div class="d-inline-flex flex-column text-center pr-3 border-right">
+                            <h6>Horario de Funcionamento:</h6>
+                            <p class="m-0">Das 8hs ás 22hs</p>
+                        </div>
+    
+                        <div class="d-inline-flex flex-column text-center pl-3">
+                            <h6>Contato:</h6>
+                            <p class="m-0">(61)9999-9999</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+    
+    <!-- Home -->
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-lg-5">
+            <a href="" class="navbar-brand d-block d-lg-none">
+                <h1 class="m-0 display-5 text-capitalize"><span class="text-primary">Amor</span> Au Pet</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
+                <div class="navbar-nav mr-auto py-0">
+                    <a href="index.php" class="nav-item nav-link active">Principal</a>                  
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Cachorro</a>
+                        <div class="dropdown-menu rounded-0 m-0">
+                            <a href="cachorro-prod-grade.php" class="dropdown-item">Grade</a>
+                            <a href="cachorro-prod-racao.php" class="dropdown-item">Ração</a>
+                            <a href="cachorro-prod-brinquedo.php" class="dropdown-item">Brinquedo</a>
+                            <a href="cachorro-prod-caminha.php" class="dropdown-item">Caminha</a>
+                        </div>
+                    </div>                      
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Gato</a>
+                        <div class="dropdown-menu rounded-0 m-0">
+                            <a href="gato-prod-grade.php" class="dropdown-item">Grade</a>
+                            <a href="gato-prod-racao.php" class="dropdown-item">Ração</a>
+                            <a href="gato-prod-brinquedo.php" class="dropdown-item">Brinquedo</a>
+                            <a href="gato-prod-caminha.php" class="dropdown-item">Caminha</a>
+                        </div>
+                    </div>      
+                    <a href="promocao.php" class="nav-item nav-link">Promoção</a>                  
+                    <a href="novidades.php" class="nav-item nav-link">Novidades</a>
+                    <a href="entrar.php" class="nav-item nav-link">Entrar</a>
+                    
+                    </div>      
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+<!-- Produtos -->
+<div class="container mt-5 pt-5 pb-3">
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq1.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 1</h5>
+                        <i>R$ 29,90</i>
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq2.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="card-body text-center p-0">
+                        <div class="team-text d-flex flex-column justify-content-center bg-light">
+                            <h5>Brinquedo 2</h5>
+                            <i>R$ 29,90</i>
+                        </div>
+                        <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                        <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq3.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 3</h5>
+                        <i>R$ 29,90</i>
+
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq4.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 4</h5>
+                        <i>R$ 29,90</i>
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div></div>
+    <div class="row">
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq5.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 5</h5>
+                        <i>R$ 29,90</i>
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq6.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="card-body text-center p-0">
+                        <div class="team-text d-flex flex-column justify-content-center bg-light">
+                            <h5>Brinquedo 6</h5>
+                            <i>R$ 29,90</i>
+                        </div>
+                        <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                        <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq7.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 7</h5>
+                        <i>R$ 29,90</i>
+
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+            <div class="team card position-relative overflow-hidden border-0 mb-4">
+                <img class="card-img-top" src="img/brinq8.jpg" alt="">
+                <div class="card-body text-center p-0">
+                    <div class="team-text d-flex flex-column justify-content-center bg-light">
+                        <h5>Brinquedo 8</h5>
+                        <i>R$ 29,90</i>
+                    </div>
+                    <div class="team-social d-flex align-items-center justify-content-center bg-dark">
+                    <a class="" href="carrinho.php">Vamos adiquirir<i class="vamos adiquirir"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div></div>
+</div>
+
+    <!-- Rodapé -->
+    <div class="container-fluid bg-dark text-white mt-5 py-5 px-sm-3 px-md-5">
+        <div class="row pt-5">
+            <div class="col-lg-4 col-md-12 mb-5">
+                <h1 class="mb-3 display-5 text-capitalize text-white"><span class="text-primary">Amor</span>Au pet</h1>
+                <p class="m-0">Fundada em 2022 em Brasilia, com objetivo de trazer mais amor e felicidade para os nossos pets, com grande diversidades de produtos e carinhos. Hoje, O amor au pet, está com uma ação de ir até você para cuidar do nossos filhotes.
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <div class="row">
+                    <div class="col-md-4 mb-5">
+                        <h5 class="text-primary mb-4">Contatos</h5>
+                        <p><i class="fa fa-map-marker-alt mr-2"></i>Universidade Católica de Brasília</p>
+                        <p><i class="fa fa-phone-alt mr-2"></i>(61)9999-9999</p>
+                    </div>
+                    <div class="col-md-4 mb-5">
+                        <h5 class="text-primary mb-4">Popular Links</h5>
+                        <div class="d-flex flex-column justify-content-start">
+                            <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Principal</a>
+                            <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Cachorro</a>
+                            <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Gato</a>
+                            <a class="text-white mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Pássaro</a>
+                            <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Novidades</a>
+                            <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Promoção</a>
+                            <a class="text-white" href="#"><i class="fa fa-angle-right mr-2"></i>Entrar</a>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-white py-4 px-sm-3 px-md-5" style="background: #111111;">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-left mb-3 mb-md-0">
+                <p class="m-0 text-white">
+                    &copy; <a class="text-white font-weight-bold"></a>Direitos Reservados a programadora Izabelle
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <a href="#" class="btn btn-lg btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
